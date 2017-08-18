84bb1f90622c3ed9968dfd19a9b7806815047aaa
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2017-08-18 01:20:04 +0000

    Searches have been calibrated for better results

--- src/bot.js
+++ src/bot.js
@@ -2,21 +2,10 @@ require('dotenv').config()
 
 const Twit = require('twit')
 const {searchTweets} = require('./actions')
+const config = require('./config')
 
-const TIME_OUT_MILESECONDS = 900000
+const twit = new Twit(config.twitter)
 
-const twit = new Twit({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: TIME_OUT_MILESECONDS
-})
-
-searchTweets(twit, {
-  q: '#nodejs, #Nodejs',
-  result_type: 'recent',
-  lang: 'en'
-})
+searchTweets(twit, config.queryStrings.retweet)
 .then(result => console.log(result))
 .catch(err => console.log(err))
