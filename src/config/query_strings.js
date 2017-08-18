fef6e8c2a5dc2b1b42d5e300f09a8d8a4bc2efe8
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2017-08-18 20:15:53 +0000

    Change the flux of setInterval

--- src/config/query_strings.js
+++ src/config/query_strings.js
@@ -1,5 +1,5 @@
 const retweet = {
-  q: '#nodejs OR #nodebots OR #nodebotssa OR #iot',
+  q: '#nodejs OR #nodebots OR #nodebotssa OR #iot OR #johnnyfivejs',
   result_type: 'recent',
   count: 100
 }
