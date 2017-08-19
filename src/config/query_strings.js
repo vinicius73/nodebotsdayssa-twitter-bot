daf9cf7d040adc0493250e63741e88c5049ba3ee
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2017-08-19 01:33:34 +0000

    change query string

--- src/config/query_strings.js
+++ src/config/query_strings.js
@@ -1,5 +1,5 @@
 const retweet = {
-  q: '#nodejs OR #nodebots OR #nodebotssa OR #iot OR #johnnyfivejs',
+  q: '#nodejs OR #nodebots OR #nodebotssa OR #johnnyfivejs',
   result_type: 'recent',
   count: 100
 }
